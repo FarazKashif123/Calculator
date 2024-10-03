777a920551f75f7e58b1ad158316addfa5b6d392
[faraz kashif123] <[farazkashif101@gmail.com]>
2024-10-03 17:03:26 +0500

    Added two functions

--- basics.py
+++ basics.py
@@ -1,3 +1,12 @@
+ #Function for addition
+def add(x, y):
+    return x + y
+
+# Function for subtraction
+def subtract(x, y):
+    return x - y
+
+
 # Function for multiplication
 def multiply(x, y):
     return x * y
